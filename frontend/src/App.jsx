9fe1e180abeef8ea1fb08d06b50022fed79b996f
Hatim bin Ashraf <hatim.ashraf91@gmail.com>
2025-03-28 22:44:55 +0100

    setting up vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='bg-blue-500 inline-block text-white px-9 py-4 m-4 rounded-md'>
+      Hello
+    </div>
+  );
+};
+
+export default App;
